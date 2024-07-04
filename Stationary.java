3a5e67f64e7d0f41a7c87da6c5f7f435fe552de8
Nandashree-V <nandashree.xworkz@gmail.com>
2024-07-04 19:59:33 +0530

    array using for loop

--- Stationary.java
+++ Stationary.java
@@ -4,7 +4,11 @@ public static void main(String store[]){
 
 String items[] = {"pencil","pen","sharpner","eraser","books","rulers","pen pencil","caryons","sketch pens","markers","jel pens","ink pens","geometry box","compass","text books"};
 
-System.out.println("available items are:" +items[0] +"," +items[1] +"," +items[2] +"," +items[3] +"," +items[4] +"," +items[5] +"," +items[6] +"," +items[7] +"," +items[8] +"," +items[9] +"," +items[10] +"," +items[11] +"," +items[12] +"," +items[13] +"," +items[14]);
+System.out.println("available items are:");
+for(String item:items){
+
+System.out.println(item);
+}
 
 
 
