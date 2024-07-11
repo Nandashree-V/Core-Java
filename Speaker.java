fc44efd8fbbba22d7d6ea428f092a961d4670e52
Nandashree-V <nandashree.xworkz@gmail.com>
2024-07-11 10:48:24 +0530

    methods using loops

--- Speaker.java
+++ Speaker.java
@@ -1,21 +1,53 @@
 class Speaker{
 
 static boolean isConnected = false;
+static int currentVolume=0;
+static int max=7;
+static int min=0;
  
 public static boolean onOrOff(){
-//System.out.println("The turnOn method Started");
+System.out.println("The turnOn method Started");
  
-    if(isConnected==false){
-	  isConnected= true;
-	 
-	}
-	else if(isConnected==true){
-		  isConnected=false;
-	
-	}
-//System.out.println("The turnOn method is ended");
+   // if(isConnected==false){
+	   if(!isConnected)  isConnected = true;
+	   else isConnected=false;	
+System.out.println("The turnOn method is ended");
 return isConnected;
 
 }
+public static void increaseVolume(){
+System.out.println("The turnOn method Started");
+if(isConnected==true){
+	currentVolume = currentVolume + 1;
+	if(currentVolume < max){
+		System.out.println(currentVolume);
+	}
+	else{
+		System.out.println("The volume reached max");
+	}
+}
+else{
+	System.out.println("Speaker is not truned on");
+}
+	
+	System.out.println("The turnOn method is ended");
+	
+}
+public static void decreaseVolume(){
+	if(isConnected=true){
+		currentVolume = currentVolume-1;
+		if(currentVolume>min){
+			System.out.println(currentVolume);
+		}
+		else{
+			System.out.println("Volume reached min");
+		}
+	}
+	else{
+		System.out.println("Speaker is not turned on");
+	}
+	
+}
 
 }
+
